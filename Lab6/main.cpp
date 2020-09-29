68de9f6d9d12226a3c0e22f90c7b8389064efb1a
Raymond <raymond13gggg@gmail.com>
2020-09-29 11:26:44 -0700

    added more comments

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -27,16 +27,16 @@ int main(){
 void transform(char *raw, char *&testStr){
     int i {0};
     int testStrLen {0};
-    while (raw[i] != '\0'){
+    while (raw[i] != '\0'){ //figure out the length of testStr pointer
         if(isalpha(raw[i]) || isdigit(raw[i])){
             ++testStrLen;
         }
         ++i;
     }
-    testStr = new char[testStrLen+1];
+    testStr = new char[testStrLen+1]; //create new char array using that length that was found above 
     int j {0};
     int k {0};
-    while (raw[j] != '\0'){
+    while (raw[j] != '\0'){ //add all alpha and numeric chars to string
         if (isalpha(raw[j]) || isdigit(raw[j])){
             testStr[k] = toupper(raw[j]);
             ++k;
@@ -44,20 +44,20 @@ void transform(char *raw, char *&testStr){
         ++j;
     }
     ++k;
-    testStr[k] = '\0'; //terminating null char
+    testStr[k] = '\0'; //terminating null char added at end of char pointer
 }
 
 bool testPalindrome(char *str){
     int pointerLen {0};
-    while (str[pointerLen] != '\0'){
+    while (str[pointerLen] != '\0'){//figure out the length of char pointer 
         ++pointerLen;
     }
-    
+
     // check to see if chars are the same forward and backwards
     for (int i {0}; i < pointerLen; ++i){
         if (str[i] != str[pointerLen - (i+1)]){
             return false;// false if linearly compared elements are not the same
         }
     }
-    return true;// value returned if arrays are both the same
+    return true;// value returned if chars are the same both ways
 }
