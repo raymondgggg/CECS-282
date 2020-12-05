1750d18cf022cc5913b146de85e182019c4244ae
Raymond <raymond13gggg@gmail.com>
2020-12-05 13:11:19 -0800

    last lab

---
+++ Lab13/main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <set>
+#include <iterator>
+using namespace std;
+
+class person{
+    private:
+        string lastName;
+        string firstName;
+        long phoneNumber;
+    public:
+        person() : lastName("blank"), firstName("blank"), phoneNumber(0L) {
+
+        } 
+        person(string lana, string fina, long pho) : lastName(lana), firstName(fina), phoneNumber(pho){
+
+        }
+        friend bool operator<(const person&, const person&);
+        friend bool operator==(const person&, const person&);
+
+        void display() const{
+            cout << endl << lastName << ",\t" << firstName
+                << "\t\tPhone: " << phoneNumber;
+        }
+
+        long get_phone(){
+            return this->phoneNumber;
+        }
+};
+
+bool operator<(const person& p1, const person& p2){
+    if(p1.lastName == p2.lastName){
+        return (p1.firstName < p2.firstName) ? true : false;
+    return (p1.lastName < p2.lastName) ? true : false;
+    }
+}
+
+bool operator==(const person& p1, const person& p2){
+     return (p1.lastName == p2.lastName && p1.firstName == p2.firstName) ? true : false;
+}
+
+class comparePersons{
+    public: 
+        bool operator()(const person* ptrP1, const person* ptrP2) const{
+            return *ptrP1 < *ptrP2;
+        }
+};
+
+class displayPerson{
+    public:
+        void operator() ( const person* ptrP) const{
+            ptrP->display();
+        }
+};
+
+void searchPerson(string lastName, string firstName, const multiset<person *, comparePersons> &people){
+    person *p1 = new person(lastName, firstName, 0L);
+    int peopleCount {0};
+    for (person *p : people){
+        if(*p1 == *p){
+            if (peopleCount == 0)
+                cout << "Results Found:";
+            p->display();
+            peopleCount++;
+        }
+    }
+    if (peopleCount == 0){
+        cout << "No one of that name was found" << endl;
+    }
+}
+ 
+int main(){
+    multiset<person *, comparePersons> people;
+
+    person *ptrP1 = new person("KuangThu", "Bruce", 4157300);
+    person *ptrP2 = new person("Deauville", "William", 8435150);
+    person *ptrP3 = new person("Wellington", "John", 9207404);
+    person *ptrP4 = new person("Bartoski", "Peter", 6946473);
+    person *ptrP5 = new person("Fredericks", "Roger", 7049982);
+    person *ptrP6 = new person("McDonald", "Stacey", 7764987);
+    person *ptrP7 = new person("KuangThu", "Bruce", 5551230);
+    person *ptrP8 = new person("Deauville", "William", 8435150);
+
+    people.insert(ptrP1);
+    people.insert(ptrP2);
+    people.insert(ptrP3);
+    people.insert(ptrP4);
+    people.insert(ptrP5);
+    people.insert(ptrP6);
+    people.insert(ptrP7);
+    people.insert(ptrP8);
+    
+    int usrInput {0};
+    cout << "1. Display people\n2. Search Person\n3. Exit program" << endl;
+    cin >> usrInput;
+
+    while (usrInput != 3){
+        if (usrInput == 1)
+        {
+            for (person *p : people)
+            {
+                p->display();
+            }
+            cout << endl;
+        }
+        else if (usrInput == 2)
+        {
+            cin.ignore();
+            string lastName;
+            string firstName;
+            cout << "Enter last name: ";
+            getline(cin, lastName);
+            cout << "Enter first name: ";
+            getline(cin, firstName);
+            searchPerson(lastName, firstName, people);
+            cout << endl << endl;
+        }
+        cout << "1. Display people\n2. Search Person\n3. Exit program" << endl;
+        cin >> usrInput;
+    }
+    return 0;
+}
